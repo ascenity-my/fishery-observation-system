d3b562328b97c2137444483238752b86c0861289
Aidil <th3ygen@gmail.com>
2022-01-31 15:52:30 +0800

    added pm2 ecosystem

---
+++ ecosystem.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    apps: [
+        {
+            name: 'sas-aqua-api',
+            script: './server.js',
+            instances: 1,
+            autorestart: true,
+        },
+        {
+            name: 'sas-aqua-react',
+            script: 'serve -s react/build',
+        }
+    ]
+}
