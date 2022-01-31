8f4a32eccb684d74aba4a507accca202fbcab730
Aidil <th3ygen@gmail.com>
2022-01-31 16:58:20 +0800

    disabled unused-var and exhaustif-dep warning

--- react/src/page/common/visualization/Visualization.jsx
+++ react/src/page/common/visualization/Visualization.jsx
@@ -1,3 +1,6 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable no-unused-vars */
+
 import { useEffect, useState } from "react";
 import { useOutletContext } from "react-router-dom";
 
