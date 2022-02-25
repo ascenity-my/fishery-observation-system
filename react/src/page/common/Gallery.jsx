52b7c9d2279bcccb6f911b994c49acbebba53ab7
Aidil <th3ygen@gmail.com>
2022-02-25 20:26:47 +0800

    page Home,About,Gallery init

---
+++ react/src/page/common/Gallery.jsx
@@ -0,0 +1,9 @@
+import styles from "styles/common/Gallery.module.scss";
+
+export default function Gallery() {
+    return (
+        <div className={styles.container}>
+            
+        </div>
+    )
+}
---
+++ react/src/page/common/Home.jsx
@@ -0,0 +1,9 @@
+import styles from "styles/common/Home.module.scss";
+
+export default function Home() {
+    return (
+        <div className={styles.container}>
+            
+        </div>
+    )
+}
