f745084536543c16c301ef0a336bac66354004cf
ZA8599 <zatyaty5258@gmail.com>
2022-03-24 01:26:27 +0800

    update orgchart

--- react/src/page/common/About.jsx
+++ react/src/page/common/About.jsx
@@ -7,16 +7,30 @@ import chartOrg from "@balkangraph/orgchart.js";
 
 export default function About() {
 
-	/*const chart = new chartOrg(document.getElement("chartOrg"), {
-        nodeBinding: {
-            field_0: "name"
-        },
-        nodes: [
-            { id: 1, name: "Amber McKenzie" },
-            { id: 2, pid: 1, name: "Ava Field" },
-            { id: 3, pid: 1, name: "Peter Stevens" }
-        ]
-    });*/
+	chartOrg = (props) =>{
+        if (typeof window === 'object') {
+            var chart = new chartOrg(document.getElementById("tree"), {
+              nodeBinding: props.nodeBinding,
+              nodes: props.nodes
+            });
+          }
+          return (null)
+    }
+
+    const data = [
+        { id: 1, name: "Denny Curtis", title: "CEO", img: "https://cdn.balkan.app/shared/2.jpg" },
+        { id: 2, pid: 1, name: "Ashley Barnett", title: "Sales Manager", img: "https://cdn.balkan.app/shared/3.jpg" },
+        { id: 3, pid: 1, name: "Caden Ellison", title: "Dev Manager", img: "https://cdn.balkan.app/shared/4.jpg" },
+        { id: 4, pid: 2, name: "Elliot Patel", title: "Sales", img: "https://cdn.balkan.app/shared/5.jpg" },
+        { id: 5, pid: 2, name: "Lynn Hussain", title: "Sales", img: "https://cdn.balkan.app/shared/6.jpg" },
+        { id: 6, pid: 3, name: "Tanner May", title: "Developer", img: "https://cdn.balkan.app/shared/7.jpg" },
+        { id: 7, pid: 3, name: "Fran Parsons", title: "Developer", img: "https://cdn.balkan.app/shared/8.jpg" }
+    ]
+
+    var nodeBinding = {
+        field_0: "name",
+        img_0: "img"
+    }
 
 	return (
 		<div className={`${styles.container}`}>
@@ -100,10 +114,12 @@ export default function About() {
 
 			<div className={styles.chartTitle}>CARTA ORGANISASI</div>
 
-			<div className={styles.chart}>
-                <chartOrg>
-                   
-                </chartOrg>
+			<div className={styles.chart} id="tree">
+
+                <chartOrg 
+                    nodes={data}
+                    nodeBinding={nodeBinding}
+                />
 			</div>
 		</div>
 	);
